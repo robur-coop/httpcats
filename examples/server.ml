e56dafd3f83dc1b536e182f17d92f7b8fd02dd47
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-08-28 11:16:14 +0200

    Fix the server example

--- examples/server.ml
+++ examples/server.ml
@@ -25,15 +25,35 @@ let () = Logs.set_reporter (reporter Fmt.stderr)
 let () = Logs_threaded.enable ()
 let () = Printexc.record_backtrace true
 
-let index_html =
-  {html|<html>
-  <head>
-    <title>httpcats</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-  </body>
-</html>|html}
+let text =
+  "CHAPTER I. Down the Rabbit-Hole  Alice was beginning to get very tired of \
+   sitting by her sister on the bank, and of having nothing to do: once or \
+   twice she had peeped into the book her sister was reading, but it had no \
+   pictures or conversations in it, <and what is the use of a book,> thought \
+   Alice <without pictures or conversations?> So she was considering in her \
+   own mind (as well as she could, for the hot day made her feel very sleepy \
+   and stupid), whether the pleasure of making a daisy-chain would be worth \
+   the trouble of getting up and picking the daisies, when suddenly a White \
+   Rabbit with pink eyes ran close by her. There was nothing so very \
+   remarkable in that; nor did Alice think it so very much out of the way to \
+   hear the Rabbit say to itself, <Oh dear! Oh dear! I shall be late!> (when \
+   she thought it over afterwards, it occurred to her that she ought to have \
+   wondered at this, but at the time it all seemed quite natural); but when \
+   the Rabbit actually took a watch out of its waistcoat-pocket, and looked at \
+   it, and then hurried on, Alice started to her feet, for it flashed across \
+   her mind that she had never before seen a rabbit with either a \
+   waistcoat-pocket, or a watch to take out of it, and burning with curiosity, \
+   she ran across the field after it, and fortunately was just in time to see \
+   it pop down a large rabbit-hole under the hedge. In another moment down \
+   went Alice after it, never once considering how in the world she was to get \
+   out again. The rabbit-hole went straight on like a tunnel for some way, and \
+   then dipped suddenly down, so suddenly that Alice had not a moment to think \
+   about stopping herself before she found herself falling down a very deep \
+   well. Either the well was very deep, or she fell very slowly, for she had \
+   plenty of time as she went down to look about her and to wonder what was \
+   going to happen next. First, she tried to look down and make out what she \
+   was coming to, but it was too dark to see anything; then she looked at the \
+   sides of the well, and noticed that they were filled with cupboards......"
 
 let error_msgf fmt = Format.kasprintf (fun msg -> Error msg) fmt
 
@@ -64,44 +84,27 @@ let sockaddr_of_arguments () =
       Format.eprintf "%s [<address>] [<port>]\n%!" Sys.executable_name;
       exit 1
 
-let listen sockaddr =
-  let file_descr =
-    match sockaddr with
-    | Unix.ADDR_INET (inet_addr, _) ->
-        if Unix.is_inet6_addr inet_addr then Miou_unix.tcpv6 ()
-        else Miou_unix.tcpv4 ()
-    | _ -> failwith "Invalid address"
-  in
-  Miou_unix.bind_and_listen file_descr sockaddr;
-  file_descr
+let resp =
+  let open H1 in
+  let headers = Headers.of_list
+    [
+      ("content-type", "text/plain; charset=utf-8")
+    ; ("content-length", string_of_int (String.length text))
+    ] in
+  Response.create ~headers `OK
 
-let rec cleanup orphans =
-  match Miou.care orphans with
-  | None | Some None -> ()
-  | Some (Some prm) -> Miou.await_exn prm; cleanup orphans
-
-let handler = function
-  | `V2 _ -> assert false
-  | `V1 reqd -> (
-      let open H1 in
-      let request = Reqd.request reqd in
-      match request.Request.target with
-      | "" | "/" | "/index.html" ->
-          let headers =
-            Headers.of_list
-              [
-                ("content-type", "text/html; charset=utf-8")
-              ; ("content-length", string_of_int (String.length index_html))
-              ]
-          in
-          let resp = Response.create ~headers `OK in
-          let body = Reqd.request_body reqd in
-          Body.Reader.close body;
-          Reqd.respond_with_string reqd resp index_html
-      | _ ->
-          let headers = Headers.of_list [ ("content-length", "0") ] in
-          let resp = Response.create ~headers `Not_found in
-          Reqd.respond_with_string reqd resp "")
+let[@warning "-8"] handler _ (`V1 reqd : [ `V1 of H1.Reqd.t | `V2 of H2.Reqd.t ]) =
+  let open H1 in
+  let request = Reqd.request reqd in
+  match request.Request.target with
+  | "" | "/" | "/index.html" ->
+      let body = Reqd.request_body reqd in
+      Body.Reader.close body;
+      Reqd.respond_with_string reqd resp text
+  | _ ->
+      let headers = Headers.of_list [ ("content-length", "0") ] in
+      let resp = Response.create ~headers `Not_found in
+      Reqd.respond_with_string reqd resp ""
 
 let server sockaddr = Httpcats.Server.clear ~handler sockaddr
 let () = Sys.set_signal Sys.sigpipe Sys.Signal_ignore
@@ -112,6 +115,7 @@ let () =
   Miou_unix.run @@ fun () ->
   let domains = Miou.Domain.available () in
   let prm = Miou.async @@ fun () -> server addr in
-  Miou.parallel server (List.init domains (Fun.const addr))
-  |> List.iter (function Ok () -> () | Error exn -> raise exn);
+  if domains > 0
+  then Miou.parallel server (List.init domains (Fun.const addr))
+       |> List.iter (function Ok () -> () | Error exn -> raise exn);
   Miou.await_exn prm
